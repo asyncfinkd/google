86c89869157d0384f041e6ef03ba90006542596d
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 16:30:29 +0400

    Header

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,14 @@ export default function Home() {
         <div className="flex space-rl-4 items-center">
           <p className="hover:underline cursor-pointer">Gmail</p>
           <p className="hover:underline cursor-pointer">Images</p>
+          <svg
+            class="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"
+            style={{ fill: "#374151" }}
+            focusable="false"
+            viewBox="0 0 24 24"
+          >
+            <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+          </svg>
           <Avatar
             url={"https://avatars.githubusercontent.com/u/80542304?v=4"}
           />
