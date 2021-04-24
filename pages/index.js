7f4f6f52d0b881124872b75a4a2468457bd9b0cd
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 15:33:34 +0400

    Header

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,16 @@ export default function Home() {
 
       {/* Header */}
 
-      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+      <header className="flex w-full p-5 justify-between text-sm color-gray-700">
         {/* Left Position */}
         <div className="flex space-rl-4 items-center">
-          <p>About</p>
-          <p>Store</p>
+          <p className="hover:underline cursor-pointer">About</p>
+          <p className="hover:underline cursor-pointer">Store</p>
         </div>
         <div className="flex space-rl-4 items-center">
-          <p>Gmail</p>
-          <p>Images</p>
+          <p className="hover:underline cursor-pointer">Gmail</p>
+          <p className="hover:underline cursor-pointer">Images</p>
+          <Avatar />
         </div>
       </header>
     </>
