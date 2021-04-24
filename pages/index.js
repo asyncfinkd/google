1283e50dab7b1cad9bde4ad7dea16996374aaa89
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 18:12:14 +0400

    Update

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
           width="300"
           height="100"
         />
-        <div className="flex w-full mt-5 hover:shadow-lg max-w-md focus-within:shadow-lg">
+        <div className="flex w-full mt-5 hover:shadow-lg max-w-md focus-within:shadow-lg rounded-full border">
           <svg
             focusable="false"
             className="h-5 mr-3"
@@ -50,7 +50,7 @@ export default function Home() {
           >
             <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
           </svg>
-          <input type="text" className="bg-red-500 focus:outline-none" />
+          <input type="text" className="flex-grow focus:outline-none" />
           <svg
             class="h-5"
             focusable="false"
