40bac05649c67005c9fdfa9b23857abb0ef4617f
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 17:49:44 +0400

    Global, Index

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,38 @@ export default function Home() {
           width="300"
           height="100"
         />
+        <div className="flex w-full mt-5 hover:shadow-lg max-w-md focus-within:shadow-lg">
+          <svg
+            focusable="false"
+            className="h-5 mr-3"
+            style={{ fill: "#6b7280" }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+          </svg>
+          <input type="text" className="bg-red-500 focus:outline-none" />
+          <svg
+            class="h-5"
+            focusable="false"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"
+              fill="#4285f4"
+            ></path>
+            <path d="m11 18.08h2v3.92h-2z" fill="#34a853"></path>
+            <path
+              d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"
+              fill="#f4b400"
+            ></path>
+            <path
+              d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"
+              fill="#ea4335"
+            ></path>
+          </svg>
+        </div>
       </form>
     </>
   );
