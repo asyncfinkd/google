aa0889da0c0f77bfe265dde9fd5ef1c511680b24
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 14:29:14 +0400

    Next JS

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Google</title>
+      </Head>
+      <div className="flex flex-column"></div>
+    </>
+  );
+}
