581c6f4180746c1461725994898240d2bb1dca53
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-29 12:07:38 +0400

    Stylesheet

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer>
+      <footer className="grid w-full divide-y-1 divide-gray-300 bg-gray-100 text-sm color-gray-500">
         <div className="plr-8 ptb-3">
           <p>Georgia</p>
+        </div>
+        <div>
+          <div>Carbon neutral since 2007</div>
         </div>
       </footer>
     </>
